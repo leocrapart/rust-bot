72826703336e8a4a5840dce6e176ad906d906049
Leo Crapart <leocrapart@yahoo.fr>
2022-09-13 01:07:56 +0200

    responding to commands and dedicated server

--- src/bin/test_token.rs
+++
@@ -1,8 +0,0 @@
-use std::env;
-use dotenv::dotenv;
-
-fn main() {
-	dotenv().ok();
-	let token = env::var("DISCORD_TOKEN").expect("token");
-	println!("{token}")
-}
--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,32 @@ use dotenv::dotenv;
 use serenity::async_trait;
 use serenity::prelude::*;
 use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{StandardFramework, CommandResult};
 
 #[group]
-#[commands(ping)]
+#[commands(plant, cut, leaderboard)]
 struct General;
 
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected", ready.user.name);
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
     dotenv().ok();
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
-    println!("{token}");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
@@ -40,8 +44,22 @@ async fn main() {
 }
 
 #[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
+async fn plant(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(ctx, "1 tree has been planted").await?;
     Ok(())
 }
+
+#[command]
+async fn cut(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(ctx, "1 tree has been cut").await?;
+    Ok(())
+}
+
+
+#[command]
+async fn leaderboard(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "1 leoo#3446             (**155**)\n2 EnzoLeDebilo#6159 (**19**)").await?;
+    Ok(())
+}
+
+
