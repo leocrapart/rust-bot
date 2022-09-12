120eee7b3ee9bfde6a1206c4f9b36026cd34d02e
Leo Crapart <leocrapart@yahoo.fr>
2022-09-13 00:07:51 +0200

    ignore .env

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use dotenv::dotenv;
 
 use serenity::async_trait;
 use serenity::prelude::*;
@@ -21,8 +22,10 @@ async fn main() {
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
+    dotenv().ok();
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
+    println!("{token}");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
